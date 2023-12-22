14806df18791c7fcb0c9dd28a02dfc5d526d99d0
Shreya Singh <146462842+singhshreya-bhriguvanshi@users.noreply.github.com>
2023-12-22 10:26:47 +0530

    Update scriptWatch.js

--- current/scriptWatch.js
+++ current/scriptWatch.js
@@ -2,9 +2,29 @@
 
 function update(){
 document.getElementById("timeHour").innerHTML= new Date().getHours();
-  document.getElementById("timeSec").innerHTML=new Date().getSeconds ();  
+  // document.getElementById("timeSec").innerHTML=new Date().getSeconds ();  
   document.getElementById("timeMin").innerHTML=new Date().getMinutes();
-  console.log("Hello")
+  // console.log("Hello")
   // setInterval(update,1000);
+  let hour2=new Date().getHours();
+  let min2=new Date().getMinutes();
+  console.log(hour2);
+  if(hour2<12)
+  {
+    console.log("AM");
+    document.getElementById("ampm").innerText=' AM';
+  }
+  else
+  {
+    console.log("PM");
+    document.getElementById("ampm").innerText=' PM';
+}
+if(min2<10)
+{
+  document.getElementById('timeMin').innerText="0"+min2;
+}
+else{
+  document.getElementById('timeMin').innerText=min2;
+}
 }
 interval = setInterval(update,1000);
