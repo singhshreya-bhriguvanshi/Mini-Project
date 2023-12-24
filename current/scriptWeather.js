d2b7b6e7120a78c4e9a8133cdaa1bb2c78a36afe
SilkiRai <silkyrai18@gmail.com>
2023-12-24 23:13:50 +0530

    modified:   current/index.html for adding weather interface 	new file:   current/scriptWeather.js 	modified:   current/styles.css for weather css

---
+++ current/scriptWeather.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+
+    const apiKey = "ddaddd18844f7bd60fff29e31d5a96f5";
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+    const searchBox = document.querySelector(".search input");
+    const searchBtn = document.querySelector(".search button");
+
+
+    async function checkWeather(city) {
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        var data = await response.json();
+
+        if (response.status == 404) {
+            document.querySelector(".error").style.display = "block";
+            document.querySelector(".weather1").style.display = "none";
+        }
+
+        else {
+            document.querySelector(".city").innerHTML = data.name;
+            document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + " °C";
+            document.querySelector(".description").innerHTML = " ("+data.weather[0].description+")";
+            // document.querySelector(".temp_feel").innerHTML=Math.round(data.main.feels_like)+" °C";  
+            document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+            document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+            document.querySelector(".weather1").style.display = "block";
+
+            document.querySelector(".error").style.display = "none";
+            document.querySelector(".weather1").style.display = "block";
+        }
+    }
+
+    searchBtn.addEventListener("click", () => {
+        checkWeather(searchBox.value)
+    });
+
+    searchBox.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            checkWeather(searchBox.value);
+        }
+    });
+
+})
