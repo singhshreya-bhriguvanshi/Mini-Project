c09b8c637e0d73e512a2dd7c97a616abafa0080f
Harsh Mishra <harsh2204mishra@gmail.com>
2023-12-28 12:27:23 +0530

    added quotation card

---
+++ current/quote.js
@@ -0,0 +1,47 @@
+var quotes = [
+  "When you have a dream, you've got to grab it and never let go.",
+  "Nothing is impossible. The word itself says 'I'm possible!",
+  "There is nothing impossible to they who will try.",
+  "The bad news is time flies. The good news is you're the pilot.",
+  "Life has got all those twists and turns. You've got to hold on tight and off you go.",
+  "Keep your face always toward the sunshine, and shadows will fall behind you.",
+  "You define your own life. Don't let other people write your script.",
+  "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+  "The road to success and the road to failure are almost exactly the same.",
+  "It is often the small steps, not the giant leaps, that bring about the most lasting change.",
+  "Education is the most powerful weapon which you can use to change the world.",
+  "There is always light. If only we're brave enough to see it. If only we're brave enough to be it.",
+  "If you want to lift yourself up, lift up someone else.",
+  "I have learned not to allow rejection to move me.",
+  "I scorched the earth with my talent and I let my light shine.",
+];
+var c = 1;
+var len = quotes.length;
+var a = document.querySelector("#quotation");
+console.log(a);
+// a.addEventListener("click", function () {
+// if (c == 4) {
+//     c = 0;
+//   }
+//   console.log(quotes[c]);
+//   a.innerHTML = quotes[c];
+//   c++;
+// });
+
+function change() {
+  if (c == len) {
+    c = 0;
+  }
+  console.log(quotes[c]);
+  a.innerHTML = '"' + quotes[c] + '"';
+  c++;
+}
+
+var myTimer = setInterval(change, 10000);
+a.addEventListener("mouseover", function () {
+  clearInterval(myTimer);
+});
+
+a.addEventListener("mouseout", function () {
+  myTimer = setInterval(change, 10000);
+});
--- current/scriptWatch.js
+++ current/scriptWatch.js
@@ -1,38 +1,31 @@
 // let time =new Date();
 
-function update(){
-document.getElementById("timeHour").innerHTML= new Date().getHours();
-  // document.getElementById("timeSec").innerHTML=new Date().getSeconds ();  
-  document.getElementById("timeMin").innerHTML=new Date().getMinutes();
+function update() {
+  document.getElementById("timeHour").innerHTML = new Date().getHours();
+  // document.getElementById("timeSec").innerHTML=new Date().getSeconds ();
+  document.getElementById("timeMin").innerHTML = new Date().getMinutes();
   // console.log("Hello")
   // setInterval(update,1000);
-  let hour2=new Date().getHours();
-  let min2=new Date().getMinutes();
-  console.log(hour2);
-  if(hour2<12)
-  {
-    console.log("AM");
-    document.getElementById("ampm").innerText=' AM';
+  let hour2 = new Date().getHours();
+  let min2 = new Date().getMinutes();
+  // console.log(hour2);
+  if (hour2 < 12) {
+    // console.log("AM");
+    document.getElementById("ampm").innerText = " AM";
+  } else {
+    // console.log("PM");
+    document.getElementById("ampm").innerText = " PM";
   }
-  else
-  {
-    console.log("PM");
-    document.getElementById("ampm").innerText=' PM';
+  if (min2 < 10) {
+    document.getElementById("timeMin").innerText = "0" + min2;
+  } else {
+    document.getElementById("timeMin").innerText = min2;
+  }
+  // let hour2=new Date().getHours();
+  if (hour2 < 10) {
+    document.getElementById("timeHour").innerText = "0" + hour2;
+  } else {
+    hour2document.getElementById("timeHour").innerText = hour2;
+  }
 }
-if(min2<10)
-{
-  document.getElementById('timeMin').innerText="0"+min2;
-}
-else{
-  document.getElementById('timeMin').innerText=min2;
-}
-// let hour2=new Date().getHours();
-if(hour2<10)
-{
-  document.getElementById('timeHour').innerText="0"+hour2;
-}
-else{
-  hour2document.getElementById('timeHour').innerText=hour2;
-}
-}
-interval = setInterval(update,1000);
+interval = setInterval(update, 1000);
