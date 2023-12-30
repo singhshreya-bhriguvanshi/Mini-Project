369323fc3faffd57d9144da2288d9246fbbdbe21
Harsh Mishra <harsh2204mishra@gmail.com>
2023-12-31 02:30:59 +0530

    modified:   current/Calculator/calc.css 	renamed:    current/Calculator/calc.html -> current/calc.html 	renamed:    current/Calculator/calc.js -> current/calc.js 	modified:   current/index.html

--- current/calc.js
+++ current/calc.js
@@ -12,6 +12,8 @@ arr.forEach((button) => {
       str = str.substring(0, str.length - 1);
     } else if (e.target.innerHTML == "AC") {
       str = "";
+    } else if (e.target.innerHTML == `<i class="fa fa-close"></i>`) {
+      window.location.href = "index.html";
     } else {
       str += e.target.innerHTML;
     }
