e6f260390efd145c200f38a2217d82648bfba89e
Harsh Mishra <harsh2204mishra@gmail.com>
2024-01-02 15:08:49 +0530

    created quotes with the api

--- current/quote.js
+++ current/quote.js
@@ -1,47 +1,28 @@
-var quotes = [
-  "When you have a dream, you've got to grab it and never let go.",
-  "Nothing is impossible. The word itself says 'I'm possible!",
-  "There is nothing impossible to they who will try.",
-  "The bad news is time flies. The good news is you're the pilot.",
-  "Life has got all those twists and turns. You've got to hold on tight and off you go.",
-  "Keep your face always toward the sunshine, and shadows will fall behind you.",
-  "You define your own life. Don't let other people write your script.",
-  "Success is not final; failure is not fatal: It is the courage to continue that counts.",
-  "The road to success and the road to failure are almost exactly the same.",
-  "It is often the small steps, not the giant leaps, that bring about the most lasting change.",
-  "Education is the most powerful weapon which you can use to change the world.",
-  "There is always light. If only we're brave enough to see it. If only we're brave enough to be it.",
-  "If you want to lift yourself up, lift up someone else.",
-  "I have learned not to allow rejection to move me.",
-  "I scorched the earth with my talent and I let my light shine.",
-];
-var c = 1;
-var len = quotes.length;
-var a = document.querySelector("#quotation");
-// console.log(a);
-// a.addEventListener("click", function () {
-// if (c == 4) {
-//     c = 0;
-//   }
-//   console.log(quotes[c]);
-//   a.innerHTML = quotes[c];
-//   c++;
-// });
+var quotation = document.querySelector("#quotation");
 
-function change() {
-  if (c == len) {
-    c = 0;
-  }
-  // console.log(quotes[c]);
-  a.innerHTML = '"' + quotes[c] + '"';
-  c++;
+async function getQuotes() {
+  var quotes = `https://api.quotable.io/random?maxLength=150`;
+  // var quotes = "https://api.humorapi.com/jokes/search";
+  //   var c = 1;
+
+  try {
+    let data = await fetch(quotes);
+    let quote = await data.json();
+    // var rn = Math.floor(Math.ramdom() * quote.length);
+    quotation.innerHTML = quote.content;
+    // author.innerHTML = quote.author;
+
+    console.log(quotes);
+  } catch (error) {}
 }
 
-var myTimer = setInterval(change, 10000);
-a.addEventListener("mouseover", function () {
+// function change() {}
+var myTimer = setInterval(getQuotes, 10000);
+
+quotation.addEventListener("mouseover", function () {
   clearInterval(myTimer);
 });
 
-a.addEventListener("mouseout", function () {
-  myTimer = setInterval(change, 10000);
+quotation.addEventListener("mouseout", function () {
+  myTimer = setInterval(getQuotes, 10000);
 });
